ab7739f2ca60fbbf5423a25c10a2ba0bcd20f714
Karl <23554+kvpb@users.noreply.github.com>
2024-12-31 23:59:59 +0100

    [cidno/ updated to version 4.21:] (cidno/src/rcg7tid/)R_IDNo.rs updated.

--- src/rcg7tid/R_IDNo.rs
+++ src/rcg7tid/R_IDNo.rs
@@ -10,18 +10,15 @@
 ////
 *///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//extern "C"
-//{
-//	uint8_t nbrlen( uint32_t, int8_t );
-//}
+#![allow(non_snake_case)]
 
 extern crate libm;
 extern crate rand;
 
-use libm::pow; //use crate::rcg7tid::R_IDNo::libm::pow; //use libm::pow;
+use libm::pow;
 use libm::floor;
-use num_traits::float; //use crate::rcg7tid::R_IDNo::libm::float; //use libm::float;
-use rand::random; //use crate::rcg7tid::R_IDNo::rand::random; //use rand::random;
+//use num_traits::float;
+//use rand::random;
 use crate::bcg7tid::l_n;
 
 pub fn R_IDNo() -> ( u16 /* TID */, u16 /* SID */, i16 /* TSV */, i8 /* TRV */, i16 /* G7SID */, i32 /* G7TID */ )
@@ -33,7 +30,7 @@ pub fn R_IDNo() -> ( u16 /* TID */, u16 /* SID */, i16 /* TSV */, i8 /* TRV */, 
 	let integer_TSV: i16;                            // trainer shiny value (TSV): 0 <= i_TSV <= ( min( 0x0, 0xFFFF ) ^ max( 0x0, 0xFFFF ) ) >> 4 == 4095, i_TSV = ( i_TID ^ i_SID ) >> 4
 	let integer_TRV: i8;                             // trainer residual value (TRV): 0 <= i_TRV <= ( min( 0x0, 0xFFFF ) ^ max( 0x0, 0xFFFF ) ) & 0xF == 15, i_TRV = ( i_TID ^ i_SID ) & 0xF
 	let integer_IDNo: u32;                           // generation VII ID number (G7ID, 'IDNo'): 0 <= i_IDNo <= 2**32 - 1 == 4294967295, i_G7ID = i_TID + ( 2**16 * i_SID )
-	let million: i32;                                // 'mask': 10**6 == 1000000
+	let million: u32;                                // 'mask': 10**6 == 1000000
 	let integer_G7TID: i32;                          // generation VII trainer ID number (G7TID): 0 <= i_G7TID <= 999999, i_G7TID = i_G7ID % 10**6
 	let radix: i8;                                   // positional numeral system: decimal system ( radix = 10 )
 	let maximum_32bitunsigned: u32;                  // 32-bit unsigned maximum value: 2**32 - 1 == 4294967295
@@ -49,25 +46,25 @@ pub fn R_IDNo() -> ( u16 /* TID */, u16 /* SID */, i16 /* TSV */, i8 /* TRV */, 
 	result = integer_TID ^ integer_SID;
 	integer_TSV = ( result >> 4 ) as i16;
 	integer_TRV = ( result & 0xF ) as i8;
-	million = pow( 10f64, 6f64 ) as i32;
+	million = 1_000_000u32; //million = pow( 10f64, 6f64 ) as i32;
 	maximum_32bitunsigned = ( pow( 2f64, 32f64 ) - 1f64 ) as u32;
 	radix = 10i8;
-	length_maximum = l_n::l_n/*nbrlen*/( maximum_32bitunsigned, radix );
+	length_maximum = l_n::l_n( maximum_32bitunsigned, radix );
 	length_G7TID = 6u8;
 	length_G7SID = ( length_maximum - length_G7TID ) as u8;
 	integer_IDNo = ( integer_TID as f64 + ( integer_SID as f64 * pow( 2f64, 16f64 ) ) ) as u32;
 	integer_G7TID = ( integer_IDNo % million as u32 ) as i32;
-	integer_G7SID = floor( integer_IDNo as f64 / pow( 10f64, ( length_maximum - length_G7SID ) as f64 )/*, 0*/ ) as i16;
+	integer_G7SID = floor( integer_IDNo as f64 / pow( 10f64, ( length_maximum - length_G7SID ) as f64 ) ) as i16;
 	tuple_IDNo = ( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
 
 	return tuple_IDNo;
 }
 
-/*//	R_IDNo.rs //R_IDNo.cpp
+/*//	R_IDNo.rs
 ////	CIDNo (IDNo Calculator)
 ////
 ////	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
 ////	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
 ////	local-part@domain
-////	https://twitter.com/ktgwkvpb
+////	https://x.com/ktgwkvpb
 *///	https://github.com/kvpb
