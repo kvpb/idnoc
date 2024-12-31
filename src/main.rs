81b2d38c3f0f029f08aa7f9f3f14a6dc8c8314a4
Karl <23554+kvpb@users.noreply.github.com>
2024-12-31 23:59:59 +0100

    [cidno/ updated to version 4.21:] (cidno/src/)main.rs updated.

--- src/main.rs
+++ src/main.rs
@@ -10,100 +10,46 @@
 ////
 *///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#![allow(unsafe_code)]
-#![allow(unused)]
-#![allow(dead_code)]
-#![allow(unused_comparisons)]
 #![allow(non_snake_case)]
-#![allow(unused_parens)]
-#![allow(warnings)]
 
-//extern crate cxx;
 extern crate libm;
 extern crate rand;
 extern crate num_traits;
-//extern crate cidno;
-//extern crate cg7tid;
-//extern crate bcg7tid;
-//extern crate rcg7tid;
 
-//#[repr(C)]
-//extern "C"
-//{
-//	fn nbrlen( number: u32, radix: i8 ) -> u8;
-//	fn f_IDNo/*IDNo_calculate*/( integer_TID: u16, integer_SID: u16 ) -> ();
-//	//fn IDNo_random_calculate() -> ();
-//}
-
-/*#[cxx::bridge]
-mod ffi
-{
-	unsafe extern "C++"
-	{
-		include!("./bin/rcg7tid/include/header.hpp");
-		fn IDNo_random_calculate() -> ();
-	}
-}*/
-
-//use cxx::*;
-//use ffi::IDNo_random_calculate;
 use std::env;
-use std::io::*; //use std::io::{stdin,stdout,Write};
 use std::str;
 use std::string::String;
-use libm::{log10,floor};
-use rand::Rng;
-use rand::thread_rng;
-use num_traits::pow;
-use std::fmt::Display;
-use std::process;
-use cidno::cg7tid::f_IDNo::f_IDNo; //use crate::cg7tid::f_IDNo::f_IDNo; //use crate::cg7tid::f_IDNo;
-use cidno::bcg7tid::F_IDNo::F_IDNo; //use crate::bcg7tid::F_IDNo::F_IDNo; //use crate::bcg7tid::{l_n,F_IDNo};
-use cidno::rcg7tid::R_IDNo::R_IDNo; //use crate::rcg7tid::R_IDNo::R_IDNo; //use crate::rcg7tid::R_IDNo;
+use cidno::cg7tid::f_IDNo::f_IDNo;
+use cidno::bcg7tid::F_IDNo::F_IDNo;
+use cidno::rcg7tid::R_IDNo::R_IDNo;
 
 fn use_help()
 {
-	print!("use: cidno [[[-]-c[alculate]] [n_TID] [n_SID]] [[[-]-b[ackward]|[-]-r[everse]] [n_G7TID]] [-R|--random[ly]]\n");
+	print!("cidno [[[-]-c[alculate]] [n_TID] [n_SID]] [[[-]-b[ackward[s]]|[-]-r[everse]] [n_G7TID]] [-R|--random[ly]]\n");
 }
 
 fn numeric_is(string: &str) -> bool
 {
 	string.chars().all( | c | c.is_numeric() )
 }
-/*fn numeric_is(string: String) -> bool
-{
-	let length: usize;
-	let mut index: usize;
-
-	length = string.chars().count();
-	index = 0;
-	while index < length
-	{
-		if string.as_str().chars().nth( index ).unwrap().is_numeric() == false
-		{
-			return false;
-		}
-	}
-	return true;
-}*/
 
 fn main()
 {
 	let vector_argument: Vec<String> = env::args().collect();
 	let count_argument: usize = vector_argument.len();
-	let mut integer_TID: u16 = 0;
-	let mut integer_SID: u16 = 0;
-	let mut integer_TSV: i16 = 0;
-	let mut integer_TRV: i8 = 0;
-	let mut integer_G7SID: i16 = 0;
-	let mut integer_G7TID: i32 = 0;
+	let mut integer_TID: u16;
+	let mut integer_SID: u16;
+	let mut integer_TSV: i16;
+	let mut integer_TRV: i8;
+	let mut integer_G7SID: i16;
+	let mut integer_G7TID: i32;
 	let mut i_index: usize;
 	let mut argument: &String;
 
 	if count_argument == 1
 	{
 		( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ) = R_IDNo();
-		print!("TID:\t{};\nSID:\t{};\nTSV:\t{};\nTRV:\t{};\nG7SID:\t{};\nG7TID:\t{};\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
+		print!("TID\t{}\nSID\t{}\nTSV\t{}\nTRV\t{}\nG7SID\t{}\nG7TID\t{}\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
 		std::process::exit( 0 );
 	}
 	i_index = 1;
@@ -112,20 +58,29 @@ fn main()
 		argument = &vector_argument[ i_index ];
 		if argument == "--calculate" || argument == "-c" || ( i_index + 1 < count_argument && numeric_is( &vector_argument[ i_index ] ) && numeric_is( &vector_argument[ i_index + 1 ] ) )
 		{
-			if numeric_is( argument )
-			{
-				integer_TID = argument.parse::<u16>().unwrap();
-				integer_SID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
-				i_index += 1;
-			}
-			else
-			{
-				integer_TID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
-				integer_SID = vector_argument[ i_index + 2 ].parse::<u16>().unwrap();
-				i_index += 2;
-			}
-			( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7TID, integer_G7SID ) = f_IDNo( integer_TID, integer_SID );
-			print!("TID:\t{};\nSID:\t{};\nTSV:\t{};\nTRV:\t{};\nG7SID:\t{};\nG7TID:\t{};\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
+			//if !numeric_is( &vector_argument[ i_index + 1 ] ) || ( i_index + 2 <= count_argument && !numeric_is( &vector_argument[ i_index + 2 ] ) ) // I failed to do it. I can't work on this at the moment. I gotta fix it later.
+			//{
+			//	eprint!("Error.\tMissing value.\nCalculating a G7ID requires inputting both a TID and a SID.\n");
+			//	use_help();
+			//	std::process::exit( 1 );
+			//}
+			//else
+			//{
+				if numeric_is( argument )
+				{
+					integer_TID = argument.parse::<u16>().unwrap();
+					integer_SID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
+					i_index += 1;
+				}
+				else
+				{
+					integer_TID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
+					integer_SID = vector_argument[ i_index + 2 ].parse::<u16>().unwrap();
+					i_index += 2;
+				}
+				( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7TID, integer_G7SID ) = f_IDNo( integer_TID, integer_SID );
+				print!("TID\t{}\nSID\t{}\nTSV\t{}\nTRV\t{}\nG7SID\t{}\nG7TID\t{}\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
+			//}
 		}
 		else if argument == "--backward" || argument == "--backwards" || argument == "-b" || argument == "--reverse" || argument == "-r"
 		{
@@ -134,17 +89,17 @@ fn main()
 				integer_G7TID = vector_argument[ i_index + 1 ].parse::<i32>().unwrap();
 				if integer_G7TID < 0 || integer_G7TID > 999999
 				{
-					eprint!("Error.\tInvalid target value.\nThe input value is not an integer number between 0 and 999999.\n");
+					eprint!("Error.\tInvalid target value.\nBackward calculating an ID requires inputting a G7TID.\n");
 					use_help();
 					std::process::exit( 1 );
 				}
-				( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID) = F_IDNo(integer_G7TID);
-				print!("G7SID:\t{};\nG7TID:\t{};\nTRV:\t{};\nTSV:\t{};\nSID:\t{};\nTID:\t{};\n", integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID );
+				( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ) = F_IDNo( integer_G7TID );
+				print!("G7SID\t{}\nG7TID\t{}\nTRV\t{}\nTSV\t{}\nSID\t{}\nTID\t{}\n", integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID );
 				i_index += 1;
 			}
 			else
 			{
-				eprint!("Error.\tMissing argument for backward calculation.\n");
+				eprint!("Error.\tMissing argument.\n");
 				use_help();
 				std::process::exit( 1 );
 			}
@@ -152,7 +107,7 @@ fn main()
 		else if argument == "--randomly" || argument == "--random" || argument == "-R"
 		{
 			( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ) = R_IDNo();
-			print!("TID:\t{};\nSID:\t{};\nTSV:\t{};\nTRV:\t{};\nG7SID:\t{};\nG7TID:\t{};\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
+			print!("TID\t{}\nSID\t{}\nTSV\t{}\nTRV\t{}\nG7SID\t{}\nG7TID\t{}\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID );
 		}
 		else if argument == "--help" || argument == "-h" || argument == "--use" || argument == "-u"
 		{
@@ -168,103 +123,6 @@ fn main()
 		i_index += 1;
 	}
 }
-/*fn main() // As usual read my routine, function and variable names backward, e.g. 'argument vector' for `vector_argument`.
-{
-	let vector_argument: Vec<String> = env::args().collect(); //dbg!( vector_argument );
-	let count_argument: usize = vector_argument.len(); //dbg!( count_argument );
-	let mut integer_TID: u16;
-	let mut integer_SID: u16;
-	let mut integer_TSV: i16;
-	let mut integer_TRV: i8;
-	let mut integer_G7SID: i16;
-	let mut integer_G7TID: i32;
-	let mut i_index: usize;
-
-	if count_argument == 1
-	{
-		( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ) = R_IDNo();
-		write!( stdout().lock(), "TID:\t{}\nSID:\t{}\nTSV:\t{}\nTRV:\t{}\nG7SID:\t{}\nG7TID:\t{}\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ).unwrap();
-		//use_help();
-		std::process::exit( 0 /*1*/ );
-	}
-	i_index = 0;
-	while i_index <= count_argument
-	{
-		//match &vector_argument[ i_index ].as_str()
-		//{
-			if vector_argument[ i_index ].as_str() == "--calculate" || vector_argument[ i_index ].as_str() == "-c" || ( numeric_is( vector_argument[ i_index ].as_str().trim().to_string() ) && numeric_is( vector_argument[ i_index + 1 ].as_str().trim().to_string() ) ) //"--calculate" | "-c" | numeric_is( vector_argument[ i_index ].as_str().trim() ) /*&vector_argument[ i_index ].as_str().trim().parse::<u16>().is_ok()*/ =>
-			{
-				if vector_argument[ i_index ].as_str().trim().parse::<u16>().is_ok()
-				{
-					integer_TID = vector_argument[ i_index /*+ 0*/ ].parse::<u16>().unwrap();
-					integer_SID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
-					/*unsafe
-					{
-						IDNo_calculate( integer_TID, integer_SID );
-					}*/
-					i_index += 1;
-				}
-				else
-				{
-					integer_TID = vector_argument[ i_index + 1 ].parse::<u16>().unwrap();
-					integer_SID = vector_argument[ i_index + 2 ].parse::<u16>().unwrap();
-					/*unsafe
-					{
-						IDNo_calculate( integer_TID, integer_SID );
-					}*/
-					i_index += 2;
-				}
-				//unsafe
-				//{
-					let ( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7TID, integer_G7SID ) = f_IDNo( integer_TID, integer_SID ); //IDNo_calculate( integer_TID, integer_SID );
-				//}
-				/*if ( &vector_argument[ i_index ].as_str().trim().parse::<u16>().is_ok() )
-				{
-					i_index += 1;
-				}
-				else
-				{
-					i_index += 2;
-				}*/
-			}//,
-			else if vector_argument[ i_index ].as_str() == "--backward" || vector_argument[ i_index ].as_str() == "-b" || vector_argument[ i_index ].as_str() == "--reverse" || vector_argument[ i_index ].as_str() == "-r" //"--backcalculate" | "-b" =>
-			{
-				integer_G7TID = vector_argument[ i_index + 1 ].parse::<i32>().unwrap();
-				if integer_G7TID/*_target*/ < 0 || 999999 < integer_G7TID/*_target*/
-				{
-					eprintln!("Error: Invalid Target Value\nThe input value is not an integer number between 0 and 999999.");
-					use_help();
-					std::process::exit( 1 );
-				}
-				//integer_G7TID = &vector_argument[ i_index + 1 ].parse::<i32>().unwrap();
-				( integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ) = F_IDNo( integer_G7TID );
-				write!( stdout().lock(), "G7SID:\t{}\nG7TID:\t{}\nTRV:\t{}\nTSV:\t{}\nSID:\t{}\nTID:\t{}\n", integer_G7SID, integer_G7TID, integer_TRV, integer_TSV, integer_SID, integer_TID ).unwrap();
-				i_index += 1;
-			}//,
-			else if vector_argument[ i_index ].as_str() == "--randomly" || vector_argument[ i_index ].as_str() == "--random" || vector_argument[ i_index ].as_str() == "-R" /*|| count_argument == 0*/ //"--random" | "-r" =>
-			{
-				( integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ) = R_IDNo();
-				write!( stdout().lock(), "TID:\t{}\nSID:\t{}\nTSV:\t{}\nTRV:\t{}\nG7SID:\t{}\nG7TID:\t{}\n", integer_TID, integer_SID, integer_TSV, integer_TRV, integer_G7SID, integer_G7TID ).unwrap();
-				/*unsafe
-				{
-					IDNo_random_calculate();
-				}*/
-				i_index += 1;
-			}//,
-			else if vector_argument[ i_index ].as_str() == "--help" || vector_argument[ i_index ].as_str() == "-h" || vector_argument[ i_index ].as_str() == "--use" || vector_argument[ i_index ].as_str() == "-u" //"--help" | "-h" =>
-			{
-				use_help();
-			}//,
-			else //_ =>
-			{
-				use_help();
-				std::process::exit( 1 );
-			}//,
-		//}
-		i_index += 1;
-	}
-	std::process::exit( 0 );
-}*/
 
 /*//	main.rs
 ////	CIDNo (IDNo Calculator)
@@ -272,7 +130,7 @@ fn main()
 ////	Karl V. P. B. `kvpb`	AKA Karl Thomas George West `ktgw`
 ////	+33 A BB BB BB BB		+1 (DDD) DDD-DDDD
 ////	local-part@domain
-////	https://twitter.com/ktgwkvpb
+////	https://x.com/ktgwkvpb
 *///	https://github.com/kvpb
 
 /*//	'[...]
